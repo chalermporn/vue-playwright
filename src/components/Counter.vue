4bfb2610acc4011e45f12c1442ca192eb2f03f96
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2023-02-12 01:00:20 +0700

    feat: playwright

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="$emit('submit', 'hello')">
+    <div id="props">{{ count }}</div>
+    <div id="remount-count">{{ remountCount }}</div>
+    <slot name="main" />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+let remountCount = 0
+</script>
+
+<script lang="ts" setup>
+defineProps<{ count?: number }>()
+
+remountCount++
+</script>
+
